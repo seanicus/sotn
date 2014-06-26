0fc21484f34854eec3984dd6a057adf73baae7ab
Sean Roberts <badger@frigiddog.com>
2014-06-26 16:38:26 -0600

    some changes to get this going again

--- js/assets/tile.js
+++ js/assets/tile.js
@@ -14,5 +14,5 @@ Game.Tile.prototype.getGlyph = function(){
 };
 
 Game.Tile.nullTile = new Game.Tile(new Game.Glyph());
-Game.Tile.floorTile = new Game.Tile(new Game.Glyph('•'));
+Game.Tile.floorTile = new Game.Tile(new Game.Glyph('-'));
 Game.Tile.wallTile = new Game.Tile(new Game.Glyph('#', 'goldenrod'));
