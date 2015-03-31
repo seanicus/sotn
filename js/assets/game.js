956f18faa2945946fd5f6d38fe4d247e7c59566f
Sean Roberts <badger@frigiddog.com>
2015-03-30 21:20:19 -0600

    just a test of git flow
    testing…

--- js/assets/game.js
+++ js/assets/game.js
@@ -6,6 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 var Game =  {
+    //init some stuff
     _mainDisplay: null,
     _displayOne: null,
     _currentScreen: null,
